1cef7f5bf52719b59d3015696784bccc559374b4
logesh002 <logeshgopu.g@gmail.com>
2022-11-25 20:00:03 +0530

    m

--- Project Development Phase/sprint4/WebApplication/ConZon_Db_connection.py
+++ Project Development Phase/sprint4/WebApplication/ConZon_Db_connection.py
@@ -1,7 +1,7 @@
 import ibm_db
 
 conn = ibm_db.connect(
-    "{IBM DB2 ODBC DRIVER};DATABASE=bludb;HOSTNAME=0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31198;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=hvs11698;PWD=xHqh4sBBGY10Ci3V",
+    "{IBM DB2 ODBC DRIVER};DATABASE=bludb;HOSTNAME=7d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31198;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=hvs11698;PWD=xHqh4sBBGY10Ci3V",
     '', '')
 
 
